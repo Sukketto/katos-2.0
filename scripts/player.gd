c95105b64316786008e6281581d212320fc0fca2
BisUmTo <bisumto@gmail.com>
2024-10-05 11:27:45 +0200

    centrata la scena

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
 func _process(delta: float) -> void:
-	var viewport_width = get_viewport_rect().size.x
-	position.x = clamp(get_viewport().get_mouse_position().x, -viewport_width/2, viewport_width/2)
+	var screen_center = get_viewport_rect().size.x/2
+	var finger_location = get_viewport().get_mouse_position().x
+	position.x = clamp(finger_location-screen_center, -540, 540)
